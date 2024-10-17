9a8b8b714c2f7920301136a1a5336566355b3037
Kamrul H Shourov <shourov.kamrul@gmail.com>
2024-10-17 16:16:00 +0600

    Sync page-icon across tabs and windows

--- toolbar-button-handler.js
+++ toolbar-button-handler.js
@@ -11,15 +11,27 @@ browser.pageAction.onClicked.addListener(async (tab) => {
         const storage = await browser.storage.local.get('visibility');
         const updatedVisibility = storage.visibility === 'hidden' ? 'visible' : 'hidden';
         await browser.storage.local.set({ 'visibility': updatedVisibility });
-        await browser.pageAction.setIcon({
-            tabId: tab.id,
-            path: {
-                16: `icons/toolbar/${updatedVisibility}/hrs-16.png`,
-                32: `icons/toolbar/${updatedVisibility}/hrs-32.png`,
-                64: `icons/toolbar/${updatedVisibility}/hrs-64.png`,
-            },
-        });
     } catch(err) {
         console.log('Error occurred when updating `visibility` property in local-storage: ', err.message);
+    }
+});
+
+browser.storage.onChanged.addListener(async () => {
+    try {
+        const storage = await browser.storage.local.get('visibility');
+        const updatedVisibility = storage.visibility;
+        const tabs = await browser.tabs.query({ url: "https://www.reddit.com/*" });
+        for (const tab of tabs) {
+            await browser.pageAction.setIcon({
+                tabId: tab.id,
+                path: {
+                    16: `icons/toolbar/${updatedVisibility}/hrs-16.png`,
+                    32: `icons/toolbar/${updatedVisibility}/hrs-32.png`,
+                    64: `icons/toolbar/${updatedVisibility}/hrs-64.png`,
+                },
+            });
+        }
+    } catch(err) {
+        console.log('Error occurred when updating icon: ', err.message);
     }
 });
