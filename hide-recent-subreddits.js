b925d4afc8ad18c1a066303e1cbe83ddc9f5bb9b
Kamrul H Shourov <shourov.kamrul@gmail.com>
2024-11-05 17:43:04 +0600

    Fix recent-subreddits hiding issue
    We have now added a observer so that any time <reddit-recent-pages>
    tag has been included, we will inject our visibility styles into it.
    
    fixes #3

--- hide-recent-subreddits.js
+++ hide-recent-subreddits.js
@@ -1,10 +1,10 @@
 (function() {
-    function getRedditRecentPagesElement() {
+    function getElement(elementName) {
         let retries = 0;
 
         return new Promise((resolve, _) => {
             function findElement() {
-                const element = document.querySelector('reddit-recent-pages');
+                const element = document.querySelector(elementName);
                 if (element) {
                     resolve(element);
                 } else if (retries < 5) {
@@ -19,25 +19,64 @@
         })
     }
 
-    async function updateVisibility() {
-        const redditRecentPages = await getRedditRecentPagesElement();
-        if (redditRecentPages) {
-            const storage = await browser.storage.local.get('visibility');
-            if (storage.visibility === 'visible') {
-                redditRecentPages.style.removeProperty('visibility');
-                redditRecentPages.style.removeProperty('display');
-            } else {
-                redditRecentPages.style.visibility = 'hidden';
-                redditRecentPages.style.display = 'none';
-            }
+    function makeNodeHidden(node) {
+        node.style.visibility = 'hidden';
+        node.style.display = 'none';
+    }
+
+    function makeNodeVisible(node) {
+        node.style.removeProperty('visibility');
+        node.style.removeProperty('display');
+    }
+
+    async function updateRecentPagesVisibility(redditRecentPages, isVisible) {
+        redditRecentPages = redditRecentPages ?? await getElement('reddit-recent-pages');
+        if (!redditRecentPages) {
+            return;
+        }
+
+        if (isVisible) {
+            makeNodeVisible(redditRecentPages);
+            // We also need to hide the <hr /> tag under the
+            // <reddit-recent-page>, otherwise there'll be
+            // one extra <hr /> tag.
+            makeNodeVisible(redditRecentPages.nextElementSibling);
+        } else {
+            makeNodeHidden(redditRecentPages);
+            makeNodeHidden(redditRecentPages.nextElementSibling);
         }
     }
 
-    window.addEventListener('load', async function() {
-        await updateVisibility();
-    });
+    async function updateVisibility(nodeName, node) {
+        const storage = await browser.storage.local.get('visibility');
+        const isVisible = storage.visibility === 'visible';
+
+        console.info(`${(new Date()).toLocaleString()}: Change visibility for ${nodeName} to ${storage.visibility}`);
+        if (nodeName === 'REDDIT-RECENT-PAGES') {
+            updateRecentPagesVisibility(node, isVisible);
+        }
+    }
+
+    const observe = (observeWindow, observeNodeName, options) => {
+        const observer = new MutationObserver(async (mutations) => {
+            for (const mutation of mutations) {
+                if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+                    for (const node of mutation.addedNodes) {
+                        if (node.nodeType === Node.ELEMENT_NODE && node.nodeName === observeNodeName) {
+                            updateVisibility(observeNodeName, node);
+                            return;
+                        }
+                    }
+                }
+            }
+        });
+
+        observer.observe(observeWindow, Object.assign({ childList: true, subtree: true }, options));
+    };
+
+    observe(document.querySelector('reddit-sidebar-nav'), 'REDDIT-RECENT-PAGES');
 
     browser.storage.onChanged.addListener(async () => {
-        await updateVisibility();
+        await updateVisibility('REDDIT-RECENT-PAGES');
     });
 })();
